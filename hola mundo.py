27e2fb834669fa9eb50631cdaace76344bf08bde
alejandrosqui <alejandrosqui080@gmail.com>
2024-06-17 02:06:16 -0300

    subo los archivos que tenía acumulados en visual

---
+++ hola mundo.py
@@ -0,0 +1,15 @@
+a=520
+b=25.333
+print (type (b))
+print (type (a))
+c="HOLA WORLD!"
+print (type (c))
+print (c[0:8])
+print (c)
+resta=a-b
+print ("la resta es: ", resta)
+division=a/b
+print ("la división es", division)
+print (a<b)
+print (b<a)
+print (a==b)    
